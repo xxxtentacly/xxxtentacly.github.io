d7b299e342e7cd779a37ca0b681ee5a05c1f412f
xxxtentacly <103616472+xxxtentacly@users.noreply.github.com>
2023-02-28 18:16:04 +0600

    Add files via upload

--- lab4/myscript.js
+++ lab4/myscript.js
@@ -1,10 +1,31 @@
-let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
-      let i = 0;
-      
-      setInterval(function() {
-        document.getElementById('myDiv').style.backgroundColor = colors[i];
-        i++;
-        if (i >= colors.length) {
-          i = 0;
-        }
-      }, 1000);
+function generateList() {
+  var items = [];
+  for (var i = 1; i <= 5; i++) {
+    items.push({
+      index: i,
+      text: "Rofl  " + i,
+      imagePath: "img" + i + ".jpg"
+    });
+  }
+  return items;
+}
+
+function createListItem(item) {
+  var li = document.createElement("li");
+  li.innerHTML = item.text;
+  var img = document.createElement("img");
+  img.src = item.imagePath;
+  li.appendChild(img);
+  return li;
+}
+
+function renderList() {
+  var list = document.getElementById("list");
+  var items = generateList();
+  for (var i = 0; i < items.length; i++) {
+    var li = createListItem(items[i]);
+    list.appendChild(li);
+  }
+}
+
+renderList();
